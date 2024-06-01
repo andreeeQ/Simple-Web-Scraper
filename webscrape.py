4136a4be1a86b1ab853009e2c997db6e15726a59
andreeeQ <eg.andrequimio@gmail.com>
2024-06-01 13:35:00 +0800

    added tag for each quotes

--- webscrape.py
+++ webscrape.py
@@ -8,13 +8,14 @@ response = requests.get(url)
 soup = BeautifulSoup(response.text,"html.parser")
 quotes = soup.findAll("span", attrs={"class":"text"})
 authors = soup.findAll("small", attrs={"class":"author"})
+tags = soup.findAll(attrs={"class":"tags"})
 
 file = open("scraped_quotes.csv","w")
 writer = csv.writer(file)
 
 writer.writerow(["QUOTES","AUTHORS"])
 
-for quote , author in zip(quotes, authors):
-    print(quote.text + " - " + author.text)
-    writer.writerow([quote.text, author.text])
+for quote , author , tag in zip(quotes, authors , tags):
+    print(quote.text + " - " + author.text + tag.text)
+    writer.writerow([quote.text, author.text,tag.text])
 file.close()
